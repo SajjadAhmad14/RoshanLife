b7605ae2149056924df3374f90c73a55d5e5a92f
Sajjad Ahmad <jogimar14@gmail.com>
2020-12-24 13:05:41 +0500

    add category_id as foreign key to articles

--- db/schema.rb
+++ db/schema.rb
@@ -1,16 +1,5 @@
-# This file is auto-generated from the current state of the database. Instead
-# of editing this file, please use the migrations feature of Active Record to
-# incrementally modify your database, and then regenerate this schema definition.
-#
-# This file is the source Rails uses to define your schema when running `rails
-# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
-# be faster and is potentially less error prone than running all of your
-# migrations from scratch. Old migrations may fail to apply correctly if those
-# migrations use external dependencies or application code.
-#
-# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_22_123012) do
+ActiveRecord::Schema.define(version: 2020_12_24_072059) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +10,8 @@ ActiveRecord::Schema.define(version: 2020_12_22_123012) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id", null: false
+    t.bigint "category_id", null: false
+    t.index ["category_id"], name: "index_articles_on_category_id"
     t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
@@ -45,6 +36,7 @@ ActiveRecord::Schema.define(version: 2020_12_22_123012) do
     t.index ["user_id"], name: "index_votes_on_user_id"
   end
 
+  add_foreign_key "articles", "categories"
   add_foreign_key "articles", "users"
   add_foreign_key "votes", "articles"
   add_foreign_key "votes", "users"
