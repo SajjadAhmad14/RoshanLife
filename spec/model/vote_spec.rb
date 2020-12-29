7eb6e83661ab4120e4b14349a074dab6098e24ea
Sajjad Ahmad <jogimar14@gmail.com>
2020-12-29 20:15:55 +0500

    add spec tests for vote model

---
+++ spec/model/vote_spec.rb
@@ -0,0 +1,13 @@
+require "rails_helper"
+
+RSpec.describe Vote, :type => :model do
+
+  it 'Vote belongs to an article' do
+    expect(Vote.reflect_on_association(:article).macro).to eq(:belongs_to)
+  end
+
+  it 'Vote belongs to a user' do
+    expect(Vote.reflect_on_association(:user).macro).to eq(:belongs_to)
+  end
+
+end
