7ff694503022d662e61a9cd45c236e848f331db7
Sajjad Ahmad <jogimar14@gmail.com>
2020-12-29 20:16:53 +0500

    install cloudinary gem and configure it to upload images

--- Gemfile
+++ Gemfile
@@ -24,6 +24,8 @@ gem 'bcrypt', '~> 3.1.7'
 
 # Use Active Storage variant
 gem 'image_processing', '~> 1.2'
+# Use Cloudinary as cloud service
+gem 'cloudinary'
 # Use gem 'dotenv-rails'
 gem 'dotenv-rails'
 # Reduces boot times through caching; required in config/boot.rb
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,7 +36,7 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
+  config.active_storage.service = :cloudinary
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
@@ -110,3 +110,4 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 end
+Rails.application.routes.default_url_options[:host] = "localhost:3000"
