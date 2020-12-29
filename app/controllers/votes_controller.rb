df7fe437550cdf3d1cc5f917fda2e583bbb40e6b
Sajjad Ahmad <jogimar14@gmail.com>
2020-12-29 13:04:19 +0500

    redirect to the same page after voting an article

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,6 +1,6 @@
 class VotesController < ApplicationController
-
   def create
-    @vote = Vote.create(user_id: params[:user_id], article_id: params[:article_id])
+    @vote = Vote.find_or_create_by(user_id: params[:user_id], article_id: params[:article_id])
+    redirect_to request.referrer, notice: "Voted"
   end
 end
