48e98e88194dce2f5f50f949e862dceb36f91d83
Sajjad Ahmad <jogimar14@gmail.com>
2020-12-30 17:26:18 +0500

    check if Article exists

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
   validates :name, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 20 }
   before_save { name.downcase! }
   has_many :articles, dependent: :destroy
-  has_many :votes
+  has_many :votes, dependent: :destroy
   has_secure_password
 end
