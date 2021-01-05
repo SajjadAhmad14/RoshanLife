dc83cfadc9af11d4997b6d8af8ca3ac395fa11a9
Sajjad Ahmad <jogimar14@gmail.com>
2021-01-05 15:20:17 +0500

    include active storage in application js file to upload images to cloud

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,7 +9,7 @@ require("@rails/activestorage").start()
 require("channels")
 import "bootstrap"
 
-//= require activestorage
+require("@rails/activestorage").start()
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
