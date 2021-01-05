610feba4782ac2883633ce8bf187f903cdbaacc0
Sajjad Ahmad <jogimar14@gmail.com>
2021-01-05 17:17:52 +0500

    delete env file

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,6 +10,7 @@ require("channels")
 import "bootstrap"
 
 require("@rails/activestorage").start()
+//= require activestorage
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
