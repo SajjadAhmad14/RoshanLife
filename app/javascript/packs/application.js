add2ad7f0894f944c592ed1fb0ff6d579544ff81
Sajjad Ahmad <jogimar14@gmail.com>
2021-01-05 20:12:52 +0500

    add cloudinary url for cloudinary as a service

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,7 +10,6 @@ require("channels")
 import "bootstrap"
 
 require("@rails/activestorage").start()
-//= require activestorage
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
